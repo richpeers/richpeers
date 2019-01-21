074ac3822cbc9b82bd41e7791cd8864e00f98801
Rich Peers <richpeers1@gmail.com>
2019-01-21 00:48:32 +0000

    wof not wof2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,8 +13,7 @@
             font-style: normal;
             font-weight: 200;
             font-display: swap;
-            src: local('Nunito ExtraLight'), local('Nunito-ExtraLight'), url(https://fonts.gstatic.com/s/nunito/v9/XRXW3I6Li01BKofA-seUYevIWzgPDA.woff2) format('woff2');
-            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+            src: local('Nunito ExtraLight'), local('Nunito-ExtraLight'), url(https://fonts.gstatic.com/s/nunito/v9/XRXW3I6Li01BKofA-seUYevO.woff) format('woff');
         }
 
         html, body {
