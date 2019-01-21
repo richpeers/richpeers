bd9d81feb99f28d4eb525f9623409d2a1a2a2c4d
Rich Peers <richpeers1@gmail.com>
2019-01-21 00:09:11 +0000

    remove analytics

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,17 +95,5 @@
     </div>
 </div>
 
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-132820171-1"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-
-    gtag('js', new Date());
-    gtag('config', 'UA-132820171-1');
-</script>
-
 </body>
 </html>
